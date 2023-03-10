94a72cbb619e43a76f654f785a7960454edcffc3
Guillermo Aviles <gjaviles7@gmail.com>
2023-03-10 16:29:55 -0500

    resize landing page img 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ce96c0ebc3513b1"; }
+/******/ 		__webpack_require__.h = function() { return "2172011e7743892e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
