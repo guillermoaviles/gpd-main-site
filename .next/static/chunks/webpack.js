df7d97f82cce22dc31540e1e59d379867aa09daa
Guillermo Aviles <gjaviles7@gmail.com>
2023-03-21 14:50:11 -0400

    switch to main

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3a71ad9782ca1d73"; }
+/******/ 		__webpack_require__.h = function() { return "b5174ac09ae6a368"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
